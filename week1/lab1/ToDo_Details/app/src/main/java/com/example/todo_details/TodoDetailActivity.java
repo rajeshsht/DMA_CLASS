14ba0dd29e5725782afc05615293284b4ef51fa5
rajeshsht <shrestharajesh55.sr@gmail.com>
2019-08-17 19:03:23 +0545

    add arrays into strings.xml

---
+++ week1/lab1/ToDo_Details/app/src/main/java/com/example/todo_details/TodoDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.todo_details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TodoDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_detail);
+    }
+}
